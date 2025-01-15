eb82bebb550e437359bede65772544c60f3448a5
NguyenHuuVietCuong-UIT <nguyenhuuvietcuong123@gmail.com>
2025-01-15 21:00:09 +0700

    cập nhật snow

--- assets/js/theme-manager.js
+++ assets/js/theme-manager.js
@@ -11,7 +11,7 @@ function getSeasonalEffect() {
     if (month >= 2 && month <= 4) return { type: 'spring', image: '/assets/logo/petal.svg' };
     if (month >= 5 && month <= 7) return { type: 'summer', image: '/assets/logo/leaf.svg' };
     if (month >= 8 && month <= 10) return { type: 'autumn', image: '/assets/logo/autumn-leaf.svg' };
-    if (month === 11 || month === 0 || month === 1) return { type: 'snow', image: 'assets/logo/snow.svg' };
+    if (month === 11 || month === 0 || month === 1) return { type: 'snow', image: '/assets/logo/snow.svg' };
     return { type: 'spring', image: 'petal.svg' };
 }
 
